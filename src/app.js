e9eb5437a56b420e53f06ff39e60fd00ec9aaeb6
José Francisco Carrión Ramos <145589365+JFCR14@users.noreply.github.com>
2025-01-03 00:46:48 +0000

    1.0

--- src/app.js
+++ src/app.js
@@ -1,11 +1,23 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+window.addEventListener = function() {
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying",
+  ];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+  let numeroRandom1 = Math.floor(Math.random() * who.length);
+  let numeroRandom2 = Math.floor(Math.random() * action.length);
+  let numeroRandom3 = Math.floor(Math.random() * what.length);
+  let numeroRandom4 = Math.floor(Math.random() * when.length);
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#excusa").innerHTML =
+    who[numeroRandom1] +
+    action[numeroRandom2] +
+    what[numeroRandom3] +
+    when[numeroRandom4];
 };
